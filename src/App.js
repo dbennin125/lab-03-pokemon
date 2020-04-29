664e28127eb9eeb7600eaa6a7878a3973c629248
Daniel Bennington <dbennington125@gmail.com>
2020-04-29 15:59:39 -0700

    search bar changes with state and verified

--- src/App.js
+++ src/App.js
@@ -5,13 +5,19 @@ import Header from './Header.js'
 
 
 export default class App extends Component {
+  //setting the state to null
+  state = {
+    searchQuery: null,
+  }
+  
   handleChange = (event) => {
     //get value of the input;
     const value = event.target.value;
-    console.log('good', value);
+    this.setState({ searchQuery: value });
   }
   
   render() {
+    console.log('|| this.state.', this.state.searchQuery)
     return (
       <div>
         <Header />
